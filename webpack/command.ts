7f75a85d1cf299c66907801f1d05ffb22f7e2dd3
zuiko <sl-xp7@bk.ru>
2022-09-03 01:20:29 +0800

    command添加注释

--- webpack/command.ts
+++ webpack/command.ts
@@ -1,20 +1,31 @@
 import { ArgsType, CommandArgs } from './common/commandArgs';
 import { program } from 'commander';
 
-/* 加载cli配置 */
-const initOption = (option) => {
+
+/**
+ * @description 加载cli参数
+ * @param {ArgsType} option 选项
+ */
+const initOption = (option:ArgsType) => {
     CommandArgs.init(option);
 };
 
-/* 加载webpack实例 */
-const initWebpack = () => {
-    /* 必须使用require */
-    const { CustomWebpack } = require('./webpack');
+/**
+ * @description 加载webpack实例
+ */
+const initWebpack = async () => {
+    /* 按需加载 */
+    const { CustomWebpack } = await import('./webpack');
     CustomWebpack.init();
 };
 
 program.version('0.0.0');
 
+/**
+ * @description cli参数类
+ * @export
+ * @class CommandOptions
+ */
 export class CommandOptions {
     static apps = program.createOption('--apps [value]');
     static env = program.createOption('--env [value]');
@@ -24,13 +35,13 @@ program
     .command('build')
     .addOption(CommandOptions.apps)
     .addOption(CommandOptions.env)
-
-    .action((args: ArgsType) => {
+    .action(async (args: ArgsType) => {
         initOption(args);
         initWebpack();
-
-        /* 必须使用require */
-        require('./index').build();
+        /* 按需加载 */
+        await import('./index').then(({ build }) => {
+            build();
+        });
     });
 
 program.parse();
